018e1975c615a1a0e09b0ce0491fafe66ffa185b
WHB901 <abdulwhbhussain@gmail.com>
2023-10-16 19:53:58 +0500

    Add the initialize Method for the item class and accessors

--- classes/item.rb
+++ classes/item.rb
@@ -1,6 +1,37 @@
+require 'date'
+
 class Item
-  def initialize()
-    # Please add your code here
+  attr_reader :archived, :genre, :author, :source, :label
+  attr_accessor :publish_date, :id
+
+  def initialize(publish_date: nil, archived: false)
+    @id = Random.rand(1..1000)
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.items << self unless genre.items.include?(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.items << self unless author.items.include?(self)
+  end
+
+  def source=(source)
+    @source = source
+    source.items << self unless source.items.include?(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.items << self unless label.items.include?(self)
   end
 
   def move_to_archive()
