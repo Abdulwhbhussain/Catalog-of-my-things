f797ecab48a0a0efc05f52679016e64f171fd5e1
WHB901 <abdulwhbhussain@gmail.com>
2023-10-17 14:37:18 +0500

    Add the Database Tables and Unit Tests

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,8 @@
 CREATE DATABASE things_catalog;
-
 CREATE TABLE genres(
   id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
   name VARCHAR(250) NOT NULL
 );
-
 CREATE TABLE music(
   id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
   author_id INT REFERENCES authors(id),
@@ -30,4 +28,20 @@ CREATE TABLE books(
   archived BOOLEAN NOT NULL,
   publisher VARCHAR(250) NOT NULL,
   cover_state VARCHAR(250) NOT NULL
+);
+CREATE TABLE authors(
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+  first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL
+);
+CREATE TABLE games(
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+  author_id INT REFERENCES authors(id),
+  genre_id INT REFERENCES genres(id),
+  source_id INT REFERENCES sources(id),
+  label_id INT REFERENCES labels(id),
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  multiplayer BOOLEAN NOT NULL,
+  last_played_at DATE NOT NULL
 );
