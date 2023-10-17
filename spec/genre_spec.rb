1717348d32979b0d2d79f5428f9737b242ef82fa
Mohammad Mahdi Niazi <m.mahdi.niazi@gmail.com>
2023-10-17 13:18:37 +0430

    Add Genre Table Add Music Table Add test for genre Add test for music

---
+++ spec/genre_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../classes/genre'
+require 'rspec'
+
+describe Genre do
+  before(:each) do
+    @genre = Genre.new(name: 'Horror')
+  end
+
+  describe '#initialize' do
+    it 'creates a new Genre object' do
+      expect(@genre).to be_an_instance_of Genre
+    end
+  end
+
+  describe '#add_item' do
+    it 'adds an item to the items array' do
+      item = double('Movie')
+      allow(item).to receive(:genre=)
+      @genre.add_item(item)
+      expect(@genre.items).to include(item)
+    end
+  end
+
+  describe '#to_json' do
+    it 'returns a JSON string' do
+      expect(@genre.to_json).to be_a String
+    end
+  end
+end
