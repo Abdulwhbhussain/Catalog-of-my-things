7a027a31deb0a6c0be80ca14ed72928ce5855cf6
MenendezON <menendezon@gmail.com>
2023-10-17 09:12:53 +0000

    Create the tables book and label into schema.sql file

--- database/schema.sql
+++ database/schema.sql
@@ -15,3 +15,19 @@ CREATE TABLE music(
   archived BOOLEAN NOT NULL,
   on_spotify BOOLEAN NOT NULL
 );
+CREATE TABLE labels(
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+  title VARCHAR(250) NOT NULL,
+  color VARCHAR(250) NOT NULL
+);
+CREATE TABLE books(
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+  author_id INT REFERENCES authors(id),
+  genre_id INT REFERENCES genres(id),
+  source_id INT REFERENCES sources(id),
+  label_id INT REFERENCES labels(id),
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
+  publisher VARCHAR(250) NOT NULL,
+  cover_state VARCHAR(250) NOT NULL
+);
